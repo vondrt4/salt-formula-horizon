5dbe0e225e53b742e1cdc795bf6e464377117fff
Tomas Vondra <vondra@czech-itc.cz>
2015-11-13 16:34:24 +0100

    Wrong file

--- horizon/files/local_settings/helpdesk_settings.py
+++ horizon/files/local_settings/helpdesk_settings.py
@@ -2,8 +2,6 @@ import os
 from django.utils.translation import ugettext_lazy as _
 from openstack_dashboard import exceptions
 
-WEBROOT = '/horizon/'
-
 {%- from "horizon/map.jinja" import server with context %}
 
 {%- if server.app is defined %}
